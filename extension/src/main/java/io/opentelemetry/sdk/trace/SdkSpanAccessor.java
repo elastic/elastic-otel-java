896cc384aa6c12bd4a0acc9c6733e105f342a1a0
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2023-09-06 14:11:11 +0200

    we can't hack access to start timestamp

--- extension/src/main/java/io/opentelemetry/sdk/trace/SdkSpanAccessor.java
+++ extension/src/main/java/io/opentelemetry/sdk/trace/SdkSpanAccessor.java
@@ -3,11 +3,8 @@ package io.opentelemetry.sdk.trace;
 public class SdkSpanAccessor {
 
     public static long getStartEpochNanos(ReadableSpan span) {
-        if (span instanceof SdkSpan) {
-            // use package-private access when we can to avoid creating single use span data
-            return ((SdkSpan) span).getStartEpochNanos();
-        } else {
-            return span.toSpanData().getStartEpochNanos();
-        }
+        // unfortunately we can't cast to SdkSpan as it's loaded in AgentClassloader and the extension is loaded
+        // in the ExtensionClassloader, hence we can't use the package-protected SdkSpan#getStartEpochNanos method
+        return span.toSpanData().getStartEpochNanos();
     }
 }
