4871065a7245667b67808375ada35e117588ce78
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2023-09-19 10:31:16 +0200

    set shadow plugin version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,6 +6,9 @@ pluginManagement {
       url = uri("https://oss.sonatype.org/content/repositories/snapshots")
     }
   }
+  plugins {
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+  }
 }
 
 rootProject.name = "elastic-otel-java"
