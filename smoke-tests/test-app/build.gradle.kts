fc3f1e3af2faab925de49223f3d5194293c5c424
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2023-09-26 11:50:08 +0200

    add profiling test endpoint

--- smoke-tests/test-app/build.gradle.kts
+++ smoke-tests/test-app/build.gradle.kts
@@ -1,7 +1,11 @@
 
-plugins {
-    java
+// Using a spring boot app is simpler for a general-purpose test app
+// - spring-boot part is already tested in the upstream agent, thus we don't have to test it again
+// - http endpoint, which is easy to call remotely
+// - implement a server, which is not a single invocation only like a CLI app
+// - multiple endpoints are possible, which allows multiple test scenarios
 
+plugins {
     id("org.springframework.boot") version "2.7.15"
     id("io.spring.dependency-management") version "1.1.3"
 }
@@ -11,24 +15,18 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     implementation("org.springframework.boot:spring-boot-starter-web")
 
+    implementation("io.opentelemetry:opentelemetry-api")
+    implementation("io.opentelemetry.instrumentation:opentelemetry-instrumentation-annotations")
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_17
 }
 
-tasks.withType<Test> {
-    useJUnitPlatform()
-}
-
 tasks {
+
+    // javadoc not required
     javadoc {
         isEnabled = false
     }
-
-    // TODO : using a spring boot app is simpler for a general-purpose app
-    // - spring-boot part is already tested in the upstream agent, thus we don't have to test it again
-    // - http endpoint, which is easy to call remotely
-    // - implement a server, which is not a single invocation only like a CLI app
-    // - multiple endpoints are possible, which allows multiple test scenarios
 }
--- smoke-tests/test-app/src/main/java/co/elastic/otel/test/AppMain.java
+++ smoke-tests/test-app/src/main/java/co/elastic/otel/test/AppMain.java
@@ -22,9 +22,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Main {
+public class AppMain {
 
   public static void main(String[] args) {
-    SpringApplication.run(Main.class, args);
+    SpringApplication.run(AppMain.class, args);
   }
 }
