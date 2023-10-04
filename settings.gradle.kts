f465669159558374a25e7320374f74823b60c6cc
SylvainJuge <763082+SylvainJuge@users.noreply.github.com>
2023-10-04 11:04:47 +0200

    dockerize test app (#10)
    * add spring-boot test app
    
    * cleanup
    
    * add profiling test endpoint
    
    * basic test for the test app
    
    * wip
    
    * catalog version change in progress
    
    * use shadow alias
    
    * remove shadow from buildSrc
    
    * wip working docker app
    
    * fix smoke test assertions & trace capture

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,11 @@
 pluginManagement {
-  repositories {
-    gradlePluginPortal()
-    maven {
-      name = "sonatype"
-      url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+    repositories {
+        gradlePluginPortal()
+        maven {
+            name = "sonatype"
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+        }
     }
-  }
-  plugins {
-    id("com.github.johnrengelman.shadow") version "8.1.1"
-  }
 }
 
 rootProject.name = "elastic-otel-java"
@@ -18,6 +15,30 @@ include("bootstrap")
 include("custom")
 include("instrumentation")
 include("smoke-tests")
+include("smoke-tests:test-app")
 include("testing:agent-for-testing")
 
-//include("test-app")
+dependencyResolutionManagement {
+    versionCatalogs {
+        create("gradlePlugins") {
+            version("shadow", "8.1.1")
+            version("jib", "3.4.0")
+            version("spotless", "6.21.0")
+
+            plugin("shadow", "com.github.johnrengelman.shadow").versionRef("shadow")
+            plugin("jib", "com.google.cloud.tools.jib").versionRef("jib")
+            plugin("spotless", "com.diffplug.spotless").versionRef("spotless")
+        }
+
+        create("libraries") {
+            version("junit", "5.10.0")
+
+            version("opentelemetrySdk", "1.29.0")
+            version("opentelemetryJavaagent", "1.30.0-SNAPSHOT")
+            version("opentelemetryJavaagentAlpha", "1.30.0-alpha-SNAPSHOT")
+
+            version("autoservice", "1.1.1")
+        }
+
+    }
+}
