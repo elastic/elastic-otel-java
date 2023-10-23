ad1a49592713741295cfbd034d77fb07d98a0c0e
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2023-10-23 17:13:36 +0200

    keep the 'all' for shaded jar for clarity

--- resources/build.gradle.kts
+++ resources/build.gradle.kts
@@ -5,17 +5,17 @@ plugins {
 }
 
 val shadowedImplementation by configurations.creating {
+  // allows to inherit common dependencies (mostly boms for now)
   extendsFrom(configurations.implementation.get())
+  // do not shadow transitive dependencies
   isTransitive = false
 }
 
 dependencies {
   // AWS cloud resource providers
   shadowedImplementation("io.opentelemetry.contrib:opentelemetry-aws-resources:" + libraries.versions.opentelemetryContribAlpha.get())
-  implementation("io.opentelemetry.contrib:opentelemetry-aws-resources:" + libraries.versions.opentelemetryContribAlpha.get())
   // application servers resource providers
   shadowedImplementation("io.opentelemetry.contrib:opentelemetry-resource-providers:" + libraries.versions.opentelemetryContribAlpha.get())
-  implementation("io.opentelemetry.contrib:opentelemetry-resource-providers:" + libraries.versions.opentelemetryContribAlpha.get())
 
   // TODO : GCP resource providers
   // "com.google.cloud.opentelemetry:detector-resources:0.25.2-alpha"
@@ -24,6 +24,20 @@ dependencies {
 
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
 }
+
+// programmatically create config for dependencies to shadow without their transitive dependencies
+//for (dep in project.configurations.getByName("implementation").allDependencies) {
+//  if (dep.group.equals("io.opentelemetry.contrib")) {
+//    shadowedImplementation.dependencies.add(dep)
+//  }
+//}
+
+// TODO : how do we expose the shadowed artifact to be used by other modules ?
+// canBeResolved = false --> define the dependency
+// canBeResolved = true --> the dependency graph can be resolved
+
+// canBeConsumed = true --> declare the configuration to be usable by other components
+// canBeConsumed = false -->
 
 tasks {
   assemble {
@@ -34,13 +48,9 @@ tasks {
     enabled = false
   }
   shadowJar {
-    archiveClassifier = null
     configurations = listOf(shadowedImplementation)
 
     // skip service provider definitions as providers should not be initialized by SDK.
     exclude("META-INF/services/**")
-
   }
 }
-
-
