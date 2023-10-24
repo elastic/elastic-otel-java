c5801517b7c4e10d6ef927ebf775d8013dd73c88
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2023-10-24 11:30:28 +0200

    use intermediate module for repacking

--- resources/build.gradle.kts
+++ resources/build.gradle.kts
@@ -4,53 +4,18 @@ plugins {
   alias(gradlePlugins.plugins.taskinfo)
 }
 
-val shadowedImplementation by configurations.creating {
-  // allows to inherit common dependencies (mostly boms for now)
-  extendsFrom(configurations.implementation.get())
-  // do not shadow transitive dependencies
-  isTransitive = false
-}
-
 dependencies {
-  // AWS cloud resource providers
-  shadowedImplementation("io.opentelemetry.contrib:opentelemetry-aws-resources:" + libraries.versions.opentelemetryContribAlpha.get())
-  // application servers resource providers
-  shadowedImplementation("io.opentelemetry.contrib:opentelemetry-resource-providers:" + libraries.versions.opentelemetryContribAlpha.get())
 
-  // TODO : GCP resource providers
-  // "com.google.cloud.opentelemetry:detector-resources:0.25.2-alpha"
-
-  // TODO : ensure the transitive dependencies of this project are preserved and the produced artifact still has them
+  implementation(project("repackaged")) {
+    attributes {
+      attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.SHADOWED))
+    }
+    dependencies {
+      // AWS cloud resource providers transitive dependencies
+      implementation("com.fasterxml.jackson.core:jackson-core:2.15.2")
+      implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    }
+  }
 
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
-}
-
-// programmatically create config for dependencies to shadow without their transitive dependencies
-//for (dep in project.configurations.getByName("implementation").allDependencies) {
-//  if (dep.group.equals("io.opentelemetry.contrib")) {
-//    shadowedImplementation.dependencies.add(dep)
-//  }
-//}
-
-// TODO : how do we expose the shadowed artifact to be used by other modules ?
-// canBeResolved = false --> define the dependency
-// canBeResolved = true --> the dependency graph can be resolved
-
-// canBeConsumed = true --> declare the configuration to be usable by other components
-// canBeConsumed = false -->
-
-tasks {
-  assemble {
-    dependsOn(shadowJar)
-  }
-  jar {
-    // jar not needed as the shadowed jar replaces it
-    enabled = false
-  }
-  shadowJar {
-    configurations = listOf(shadowedImplementation)
-
-    // skip service provider definitions as providers should not be initialized by SDK.
-    exclude("META-INF/services/**")
-  }
 }
