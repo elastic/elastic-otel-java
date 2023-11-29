4c591bfa35d60dac3f4b6d2250c452c134923a63
SylvainJuge <763082+SylvainJuge@users.noreply.github.com>
2023-11-29 13:18:01 +0100

    Update upstream agent (#49)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,27 +23,11 @@ include("testing:agent-for-testing")
 
 dependencyResolutionManagement {
     versionCatalogs {
-        create("gradlePlugins") {
-            version("shadow", "8.1.1")
-            version("jib", "3.4.0")
-            version("spotless", "6.21.0")
-
-            plugin("shadow", "com.github.johnrengelman.shadow").versionRef("shadow")
-            plugin("jib", "com.google.cloud.tools.jib").versionRef("jib")
-            plugin("spotless", "com.diffplug.spotless").versionRef("spotless")
-            plugin("taskinfo", "org.barfuin.gradle.taskinfo").version("2.1.0")
+        create("catalog") {
+          // the version catalog is currently limited to a single file due to dependabot
+          // see https://github.com/dependabot/dependabot-core/issues/8079 for details.
+          // Also, only the 'gradle/libs.versions.toml' path is supported for now.
+          from(files("gradle/libs.versions.toml"))
         }
-
-        create("libraries") {
-            version("junit", "5.10.0")
-
-            version("opentelemetrySdk", "1.29.0")
-            version("opentelemetryJavaagent", "1.30.0")
-            version("opentelemetryJavaagentAlpha", "1.30.0-alpha")
-            version("opentelemetryContribAlpha", "1.30.0-alpha")
-
-            version("autoservice", "1.1.1")
-        }
-
     }
 }
