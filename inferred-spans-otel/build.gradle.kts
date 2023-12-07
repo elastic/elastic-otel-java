23d9763d52845a4c005819ec7d206dc6dc3f8376
Jonas Kunz <jonas.kunz@elastic.co>
2023-12-07 12:05:08 +0100

    Centralized a few dependencies which are likely to be shared

--- inferred-spans-otel/build.gradle.kts
+++ inferred-spans-otel/build.gradle.kts
@@ -4,17 +4,17 @@ plugins {
 
 dependencies {
   compileOnly("io.opentelemetry:opentelemetry-sdk")
-  compileOnly("com.google.code.findbugs:jsr305:3.0.0")
+  compileOnly(libs.findbugs.jsr305)
   implementation("com.lmax:disruptor:3.4.4")
   implementation("org.jctools:jctools-core:4.0.1")
   implementation("com.blogspot.mydailyjava:weak-lock-free:0.18")
 
-  testCompileOnly("com.google.code.findbugs:jsr305:3.0.0")
+  testCompileOnly(libs.findbugs.jsr305)
   testImplementation("io.opentelemetry:opentelemetry-sdk")
   testImplementation("io.opentelemetry:opentelemetry-sdk-testing")
-  testImplementation("org.assertj:assertj-core:3.24.2")
+  testImplementation(libs.assertJ.core)
+  testImplementation(libs.awaitility)
   testImplementation("org.kohsuke:github-api:1.133")
-  testImplementation("org.awaitility:awaitility:4.2.0")
   testImplementation("org.apache.commons:commons-compress:1.21")
   testImplementation("tools.profiler:async-profiler:1.8.3")
 }
