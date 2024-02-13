17150e0fa2715ed984e95d1cf4f2fdc7c928be7d
Jonas Kunz <jonas.kunz@elastic.co>
2024-02-13 12:11:31 +0100

    Removed MethodHandle cast

--- common/src/main/java/io/opentelemetry/sdk/trace/FieldBackedSpanValueStorageProvider.java
+++ common/src/main/java/io/opentelemetry/sdk/trace/FieldBackedSpanValueStorageProvider.java
@@ -5,13 +5,13 @@ import co.elastic.otel.common.SpanValueStorageProvider;
 import io.opentelemetry.api.trace.Span;
 import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.lang.reflect.Field;
 import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.annotation.Nullable;
 
+@SuppressWarnings("unchecked")
 public class FieldBackedSpanValueStorageProvider implements SpanValueStorageProvider {
 
   private static final String FIELD_NAME = "$elasticSpanValues";
@@ -21,21 +21,20 @@ public class FieldBackedSpanValueStorageProvider implements SpanValueStorageProv
   public static final FieldBackedSpanValueStorageProvider INSTANCE;
 
   private static final MethodHandle spanFieldGetter;
-  private static final AtomicReferenceFieldUpdater<Span, Object> spanFieldSetter;
+  private static final AtomicReferenceFieldUpdater<SdkSpan, Object> spanFieldSetter;
 
   static {
     FieldBackedSpanValueStorageProvider resultInstance = null;
     MethodHandle getter = null;
-    AtomicReferenceFieldUpdater<Span, Object> setter = null;
+    AtomicReferenceFieldUpdater<SdkSpan, Object> setter = null;
     try {
-      Field storageField = SDK_SPAN_CLASS.getDeclaredField(FIELD_NAME);
+      Field storageField = SdkSpan.class.getDeclaredField(FIELD_NAME);
+      if (storageField.getType() != Object.class) {
+        throw new IllegalStateException("Unexpected field type: " + storageField.getType());
+      }
 
-      getter = MethodHandles.lookup()
-          .unreflectGetter(storageField)
-          .asType(MethodType.methodType(SpanValueStorage.class, Span.class));
-
-      setter = (AtomicReferenceFieldUpdater<Span, Object>) AtomicReferenceFieldUpdater.newUpdater(
-          SDK_SPAN_CLASS, Object.class, FIELD_NAME);
+      getter = MethodHandles.lookup().unreflectGetter(storageField);
+      setter = AtomicReferenceFieldUpdater.newUpdater(SdkSpan.class, Object.class, FIELD_NAME);
 
       logger.log(Level.FINE, "Using field-backed storage for SpanValues", FIELD_NAME);
       resultInstance = new FieldBackedSpanValueStorageProvider();
@@ -57,20 +56,22 @@ public class FieldBackedSpanValueStorageProvider implements SpanValueStorageProv
   @Nullable
   @Override
   public SpanValueStorage get(Span span, boolean initialize) {
-    SpanValueStorage value;
+    SdkSpan sdkSpan = (SdkSpan) span;
+
+    SpanValueStorage value = getFieldValue(sdkSpan);
+    if (value == null && initialize) {
+      spanFieldSetter.compareAndSet(sdkSpan, null, new SpanValueStorage());
+      value = getFieldValue(sdkSpan);
+    }
+    return value;
+  }
+
+  private static SpanValueStorage getFieldValue(SdkSpan sdkSpan) {
     try {
-      value = (SpanValueStorage) spanFieldGetter.invokeExact(span);
+      //double cast is required here because invokeExact is signature polymorphic
+      return (SpanValueStorage) (Object) spanFieldGetter.invokeExact(sdkSpan);
     } catch (Throwable e) {
       throw new IllegalStateException(e);
     }
-    if (value == null && initialize) {
-      spanFieldSetter.compareAndSet(span, null, new SpanValueStorage());
-      try {
-        value = (SpanValueStorage) spanFieldGetter.invokeExact(span);
-      } catch (Throwable e) {
-        throw new IllegalStateException(e);
-      }
-    }
-    return value;
   }
 }
