b2c8b1e2ba123a53624f07ead6b3af74d211fcc1
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-03-13 18:36:22 +0100

    simple shell script for update

---
+++ .ci/update-upstream.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+set -x
+
+upstreamRef="${1:-main}"
+
+upstream_base_url="https://raw.githubusercontent.com/open-telemetry/opentelemetry-java-instrumentation/${upstreamRef}"
+project_root="$(dirname "${0}")/.."
+
+upstream_version() {
+    set +e
+    curl -s "${upstream_base_url}/${1}" \
+        | grep "val.*${2}.*=" \
+        | sed 's/.*=//' \
+        | tr -d '"' \
+        | tr -d '[:blank:]' \
+        || echo 'unknown'
+    set -e
+}
+
+upstreamAgentVersion="$(upstream_version 'version.gradle.kts' stableVersion)"
+upstreamAgentAlphaVersion="$(upstream_version 'version.gradle.kts' alphaVersion)"
+upstreamSdkVersion="$(upstream_version 'dependencyManagement/build.gradle.kts' otelSdkVersion)"
+upstreamContribVersion="$(upstream_version 'dependencyManagement/build.gradle.kts' otelContribVersion)"
+
+sed -i "s/^openTelemetrySdk = \".*/openTelemetrySdk = \"${upstreamSdkVersion}\"/" "${project_root}/gradle/libs.versions.toml"
+sed -i "s/^opentelemetryJavaagent = \".*/opentelemetryJavaagent = \"${upstreamAgentVersion}\"/" "${project_root}/gradle/libs.versions.toml"
+sed -i "s/^opentelemetryJavaagentAlpha = \".*/opentelemetryJavaagentAlpha = \"${upstreamAgentAlphaVersion}\"/" "${project_root}/gradle/libs.versions.toml"
+sed -i "s/^opentelemetryContribAlpha = \".*/opentelemetryContribAlpha = \"${upstreamContribVersion}\"/" "${project_root}/gradle/libs.versions.toml"
+
