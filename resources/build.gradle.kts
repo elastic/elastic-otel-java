c396fe0841a6b6268f268539eae114e9b78319be
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-03-26 10:12:12 +0100

    add few comments

--- resources/build.gradle.kts
+++ resources/build.gradle.kts
@@ -8,8 +8,10 @@ dependencies {
 
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
 
+  // those are already included in the upstream agent
   compileOnly(catalog.gcpContribResources)
   compileOnly(catalog.awsContribResources)
 
+  // not included in the upstream agent
   implementation(catalog.contribResources)
 }
