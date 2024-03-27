5710a72ccd1f3c45861d6ca93cdd1754910c61e2
SylvainJuge <763082+SylvainJuge@users.noreply.github.com>
2024-03-27 17:33:16 +0100

    update to upstream 2.2.0 agent (#184)

--- resources/build.gradle.kts
+++ resources/build.gradle.kts
@@ -6,20 +6,12 @@ plugins {
 
 dependencies {
 
-  implementation(project("repackaged")) {
-    attributes {
-      attribute(Bundling.BUNDLING_ATTRIBUTE, objects.named(Bundling.SHADOWED))
-    }
-    dependencies {
-      // AWS cloud resource providers transitive dependencies
-      implementation(catalog.jackson)
-      implementation(catalog.okhttp)
-
-      // required to make the IDE compile our own resource provider, won't be included as dependency
-      compileOnly(catalog.awsContribResources)
-      compileOnly(catalog.contribResources)
-    }
-  }
-
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
+
+  // those are already included in the upstream agent
+  compileOnly(catalog.gcpContribResources)
+  compileOnly(catalog.awsContribResources)
+
+  // not included in the upstream agent
+  implementation(catalog.contribResources)
 }
--- resources/src/main/java/co/elastic/otel/resources/ElasticResourceProvider.java
+++ resources/src/main/java/co/elastic/otel/resources/ElasticResourceProvider.java
@@ -23,6 +23,7 @@ import io.opentelemetry.contrib.aws.resource.Ec2ResourceProvider;
 import io.opentelemetry.contrib.aws.resource.EcsResourceProvider;
 import io.opentelemetry.contrib.aws.resource.EksResourceProvider;
 import io.opentelemetry.contrib.aws.resource.LambdaResourceProvider;
+import io.opentelemetry.contrib.gcp.resource.GCPResourceProvider;
 import io.opentelemetry.contrib.resourceproviders.AppServerServiceNameProvider;
 import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import io.opentelemetry.sdk.autoconfigure.spi.ResourceProvider;
@@ -32,43 +33,40 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Provider for resource attributes, implements {@link ResourceProvider} but is not invoked through
+ * SPI interface but explicitly.
+ */
 public class ElasticResourceProvider implements ResourceProvider {
 
   private static final Logger logger = Logger.getLogger(ElasticResourceProvider.class.getName());
 
-  private final boolean withExtra;
+  public static String SKIP_EXPENSIVE_RESOURCE_PROVIDERS =
+      "elastic.internal.skip_expensive_resource_providers";
 
-  private ConfigProperties config;
-
-  public ElasticResourceProvider() {
-    this(true);
-  }
-
-  public ElasticResourceProvider(boolean withExtra) {
-    this.withExtra = withExtra;
-  }
+  public ElasticResourceProvider() {}
 
   @Override
   public Resource createResource(ConfigProperties config) {
-    this.config = config;
-    if (!withExtra) {
+    if (config.getBoolean(SKIP_EXPENSIVE_RESOURCE_PROVIDERS, false)) {
       return getBaseResource(config);
     }
-    return getBaseResource(config).merge(getExtraResource());
+    return getBaseResource(config).merge(getExtraResource(config));
   }
 
   private Resource getBaseResource(ConfigProperties config) {
     // application server providers : file parsing only thus fast
-    return invokeResourceProvider(new AppServerServiceNameProvider());
+    return invokeResourceProvider(new AppServerServiceNameProvider(), config);
   }
 
   /**
    * @return extra resource attributes that are expected to take some time due to making requests to
    *     external systems
    */
-  public Resource getExtraResource() {
+  public Resource getExtraResource(ConfigProperties config) {
     List<ResourceProvider> providers =
         Arrays.asList(
+            // -- AWS --
             // ec2 relies on http calls without pre-checks
             new Ec2ResourceProvider(),
             // beanstalk relies on json config file parsing
@@ -78,22 +76,30 @@ public class ElasticResourceProvider implements ResourceProvider {
             // relies on http call with url provided through env var used as pre-check
             new EcsResourceProvider(),
             // relies on env variables only
-            new LambdaResourceProvider());
+            new LambdaResourceProvider(),
+            // -- GCP --
+            new GCPResourceProvider());
     Resource resource = Resource.empty();
     for (ResourceProvider provider : providers) {
-      resource = resource.merge(invokeResourceProvider(provider));
+      resource = resource.merge(invokeResourceProvider(provider, config));
     }
     return resource;
   }
 
-  private Resource invokeResourceProvider(ResourceProvider provider) {
+  private static Resource invokeResourceProvider(
+      ResourceProvider provider, ConfigProperties config) {
+
     try {
+      logger.log(
+          Level.FINE, String.format("before invoke resource provider %s", provider.getClass()));
       Resource result = provider.createResource(config);
+      logger.log(
+          Level.FINE, String.format("after invoke resource provider %s", provider.getClass()));
       if (Resource.empty().equals(result)) {
         logger.log(
             Level.FINE,
             String.format(
-                "resource provided did not provide any attribute: %s", provider.getClass()));
+                "resource provider did not provide any attribute: %s", provider.getClass()));
       }
       return result;
     } catch (RuntimeException e) {
