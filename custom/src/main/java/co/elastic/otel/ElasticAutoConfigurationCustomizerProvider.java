c1f0af9887916104eeef2024ce6e64b83a651394
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2024-07-12 15:10:35 +0200

    add config tests

--- custom/src/main/java/co/elastic/otel/ElasticAutoConfigurationCustomizerProvider.java
+++ custom/src/main/java/co/elastic/otel/ElasticAutoConfigurationCustomizerProvider.java
@@ -21,13 +21,14 @@ package co.elastic.otel;
 import com.google.auto.service.AutoService;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizer;
 import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import io.opentelemetry.sdk.autoconfigure.spi.ConfigProperties;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 @AutoService(AutoConfigurationCustomizerProvider.class)
-public class ElasticAutoConfigurationCustomizerprovider
+public class ElasticAutoConfigurationCustomizerProvider
     implements AutoConfigurationCustomizerProvider {
 
   private static final String DISABLED_RESOURCE_PROVIDERS = "otel.java.disabled.resource.providers";
@@ -43,30 +44,31 @@ public class ElasticAutoConfigurationCustomizerprovider
                 // span processor registration
                 sdkTracerProviderBuilder.addSpanProcessor(
                     ElasticExtension.INSTANCE.getSpanProcessor()))
-        .addPropertiesCustomizer(
-            configProperties -> {
-              Set<String> disabledResourceProviders =
-                  new HashSet<>(configProperties.getList(DISABLED_RESOURCE_PROVIDERS));
-
-              // disable upstream distro name & version provider
-              disabledResourceProviders.add(
-                  "io.opentelemetry.javaagent.tooling.DistroVersionResourceProvider");
-
-              Map<String, String> config = new HashMap<>();
-
-              // enable experimental telemetry by default if not explicitly disabled
-              boolean experimentalTelemetry =
-                  configProperties.getBoolean(RUNTIME_EXPERIMENTAL_TELEMETRY, true);
-              config.put(RUNTIME_EXPERIMENTAL_TELEMETRY, Boolean.toString(experimentalTelemetry));
-
-              config.put(DISABLED_RESOURCE_PROVIDERS, String.join(",", disabledResourceProviders));
-
-              return config;
-            })
+        .addPropertiesCustomizer(ElasticAutoConfigurationCustomizerProvider::propertiesCustomizer)
         .addSpanExporterCustomizer(
             (spanExporter, configProperties) ->
                 // wrap the original span exporter
                 ElasticExtension.INSTANCE.wrapSpanExporter(spanExporter))
         .addMetricExporterCustomizer((exporter, config) -> new ElasticMetricExporter(exporter));
+  }
+
+  static Map<String, String> propertiesCustomizer(ConfigProperties configProperties) {
+    Set<String> disabledResourceProviders =
+        new HashSet<>(configProperties.getList(DISABLED_RESOURCE_PROVIDERS));
+
+    // disable upstream distro name & version provider
+    disabledResourceProviders.add(
+        "io.opentelemetry.javaagent.tooling.DistroVersionResourceProvider");
+
+    Map<String, String> config = new HashMap<>();
+
+    // enable experimental telemetry metrics by default if not explicitly disabled
+    boolean experimentalTelemetry =
+        configProperties.getBoolean(RUNTIME_EXPERIMENTAL_TELEMETRY, true);
+    config.put(RUNTIME_EXPERIMENTAL_TELEMETRY, Boolean.toString(experimentalTelemetry));
+
+    config.put(DISABLED_RESOURCE_PROVIDERS, String.join(",", disabledResourceProviders));
+
+    return config;
   }
 }
