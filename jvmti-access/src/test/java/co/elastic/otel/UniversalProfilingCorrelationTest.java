363b3ecb87e0c692f097cc5dc138918dbfc82b54
Jonas Kunz <jonas.kunz@elastic.co>
2024-07-12 12:06:02 +0200

    Disable other tests to analyze side effects

--- jvmti-access/src/test/java/co/elastic/otel/UniversalProfilingCorrelationTest.java
+++ jvmti-access/src/test/java/co/elastic/otel/UniversalProfilingCorrelationTest.java
@@ -177,7 +177,7 @@ public class UniversalProfilingCorrelationTest {
       }
     }
 
-    @Test
+    //@Test
     public void enablingVirtualThreadSupportDoesNotThrow() {
       UniversalProfilingCorrelation.setVirtualThreadSupportEnabled(true);
     }
@@ -200,7 +200,7 @@ public class UniversalProfilingCorrelationTest {
           .get();
     }
 
-    @Test
+    //@Test
     @EnabledForJreRange(min = JRE.JAVA_21)
     public void testVirtualThreadsStoragePropagatedWithMounts() throws Exception {
       ExecutorService exec =
@@ -249,7 +249,7 @@ public class UniversalProfilingCorrelationTest {
               () ->
                   virtualThreads.size() == threadLatches.size()
                       && virtualThreads.stream()
-                          .allMatch(t -> t.getState() == Thread.State.WAITING));
+                      .allMatch(t -> t.getState() == Thread.State.WAITING));
 
       // resume all threads
       for (CountDownLatch latch : threadLatches) {
@@ -278,7 +278,7 @@ public class UniversalProfilingCorrelationTest {
         name.append("abc");
       }
       assertThatThrownBy(
-              () -> UniversalProfilingCorrelation.startProfilerReturnChannel(name.toString()))
+          () -> UniversalProfilingCorrelation.startProfilerReturnChannel(name.toString()))
           .isInstanceOf(RuntimeException.class)
           .hasMessageContaining("filepath");
     }
