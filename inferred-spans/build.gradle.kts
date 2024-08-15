b19a05a43cb075934462f60437609bbd5117a8eb
Jonas Kunz <jonas.kunz@elastic.co>
2024-08-15 12:51:00 +0200

    DRY: Move autoservice and common test dependency to convention

--- inferred-spans/build.gradle.kts
+++ inferred-spans/build.gradle.kts
@@ -6,24 +6,17 @@ plugins {
 description = "Elastic Inferred Spans extension for OpenTelemetry Java"
 
 dependencies {
-  annotationProcessor(libs.autoservice.processor)
-  compileOnly(libs.autoservice.annotations)
   compileOnly("io.opentelemetry:opentelemetry-sdk")
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure-spi")
-  compileOnly(libs.findbugs.jsr305)
   implementation(libs.lmax.disruptor)
   implementation(libs.jctools)
   implementation(libs.asyncprofiler)
   implementation(libs.bundles.semconv)
   implementation(project(":common"))
 
-  testAnnotationProcessor(libs.autoservice.processor)
-  testCompileOnly(libs.autoservice.annotations)
-  testCompileOnly(libs.findbugs.jsr305)
   testImplementation(project(":testing-common"))
   testImplementation("io.opentelemetry:opentelemetry-sdk")
   testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
-  testImplementation(libs.awaitility)
   testImplementation(libs.github.api)
   testImplementation(libs.apachecommons.compress)
   testImplementation(libs.bundles.semconv)
--- resources/build.gradle.kts
+++ resources/build.gradle.kts
@@ -4,16 +4,9 @@ plugins {
 }
 
 dependencies {
-
   compileOnly("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
   testImplementation("io.opentelemetry:opentelemetry-sdk-extension-autoconfigure")
 
-  // auto-service
-  annotationProcessor(libs.autoservice.processor)
-  compileOnly(libs.autoservice.annotations)
-
   // not included in the upstream agent
   implementation(catalog.contribResources)
-
-  testImplementation(libs.assertj.core)
 }
