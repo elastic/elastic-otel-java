48b15666f098a4e57656536248a18c26197c6e58
Jonas Kunz <jonas.kunz@elastic.co>
2024-09-19 09:47:12 +0200

    Fail build if non-approved licenses are detected

--- buildSrc/src/main/kotlin/elastic-otel.license-report-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/elastic-otel.license-report-conventions.gradle.kts
@@ -2,6 +2,7 @@ import org.gradle.api.tasks.Copy
 import org.gradle.kotlin.dsl.register
 import com.github.jk1.license.filter.LicenseBundleNormalizer
 import com.github.jk1.license.render.InventoryMarkdownReportRenderer
+import com.github.jk1.license.render.ReportRenderer
 
 plugins {
   id("com.github.jk1.dependency-license-report")
@@ -9,19 +10,25 @@ plugins {
 
 val fullLicenseReport = tasks.register("fullLicenseReport", Copy::class) {
   dependsOn(tasks.generateLicenseReport)
+  //We want to fail in case there are any non-approved licenses
+  dependsOn(tasks.checkLicense)
+
   from(rootProject.rootDir) {
     include("NOTICE", "LICENSE")
   }
   from(licenseReport.outputDir) {
     include("**/*")
+    exclude("dependencies-without-allowed-license.json")
+    exclude("project-licenses-for-check-license-task.json")
     into("licenses")
   }
   into(layout.buildDirectory.dir("reports/all-licenses"))
 }
 
 licenseReport {
+  allowedLicensesFile = File("${rootProject.rootDir}/buildscripts/allowed-licenses.json")
 
-  renderers = arrayOf(InventoryMarkdownReportRenderer("more-licences.md"))
+  renderers = arrayOf<ReportRenderer>(InventoryMarkdownReportRenderer("more-licences.md"))
   excludeBoms = true
   excludes = arrayOf(
     "io.opentelemetry:opentelemetry-bom-alpha",
