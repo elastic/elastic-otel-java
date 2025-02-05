16d9ec0d7640b5bf46f32a0337c1d595a0126be3
Jonas Kunz <jonas.kunz@elastic.co>
2025-02-05 12:40:18 +0100

    line break

--- instrumentation/openai-client-instrumentation/testing-common/build.gradle.kts
+++ instrumentation/openai-client-instrumentation/testing-common/build.gradle.kts
@@ -5,9 +5,9 @@ plugins {
 dependencies {
   compileOnly(catalog.openaiClient)
   compileOnly(project(":instrumentation:openai-client-instrumentation:common"))
+
   implementation("io.opentelemetry.javaagent:opentelemetry-testing-common")
   implementation("io.opentelemetry:opentelemetry-sdk-testing")
-
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.18.2")
   implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.18.2")
   implementation("org.slf4j:slf4j-simple:2.0.16")
