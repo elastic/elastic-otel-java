e2305d67db883445f05e793404467632864ec3e4
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-03-21 10:36:29 +0100

    bootstrap troubleshooting example

---
+++ examples/troubleshooting/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+  application
+  java
+
+  id("com.gradleup.shadow") version "8.3.5"
+}
+
+group = "troubleshooting.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
