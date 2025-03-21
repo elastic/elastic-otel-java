4d52bf4b4cc0f9b546f473c86dc48bb8f1ad6325
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-03-21 16:05:13 +0100

    add logs + easier build

--- examples/troubleshooting/build.gradle.kts
+++ examples/troubleshooting/build.gradle.kts
@@ -11,3 +11,23 @@ version = "1.0-SNAPSHOT"
 repositories {
   mavenCentral()
 }
+
+dependencies {
+  implementation(platform("io.opentelemetry:opentelemetry-bom:1.48.0"))
+
+  implementation("io.opentelemetry:opentelemetry-api")
+  implementation("io.opentelemetry:opentelemetry-sdk")
+  implementation("io.opentelemetry:opentelemetry-exporter-otlp")
+}
+
+application {
+  mainClass = "elastic.troubleshooting.Main"
+}
+
+tasks {
+  shadowJar {
+    archiveFileName.set("troubleshooting.jar")
+  }
+}
+
+
