1b19ddca5bdd53f214fc5e6434ccc2355c61f4f7
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-06-26 16:42:25 +0200

    add baggage in the gateway

--- examples/baggage/build.gradle.kts
+++ examples/baggage/build.gradle.kts
@@ -16,6 +16,10 @@ dependencies {
   implementation("co.elastic.otel:elastic-otel-runtime-attach:1.4.1")
   implementation(platform("org.slf4j:slf4j-bom:2.0.16"))
   implementation("org.slf4j:slf4j-simple")
+
+  // otel API to access Baggage API
+  implementation("io.opentelemetry:opentelemetry-api:1.51.0")
+
 }
 
 application {
--- examples/baggage/src/main/java/baggage/example/SimpleServer.java
+++ examples/baggage/src/main/java/baggage/example/SimpleServer.java
@@ -1,10 +1,18 @@
 package baggage.example;
 
+import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.concurrent.Executors;
 
 public class SimpleServer {
@@ -33,17 +41,63 @@ public class SimpleServer {
       String customerId = exchange.getRequestHeaders().getFirst("customer_id");
 
       String response = String.format("hello from backend, customer_id=%s\n", customerId);
-      exchange.sendResponseHeaders(200, response.length());
-      OutputStream os = exchange.getResponseBody();
-      os.write(response.getBytes());
-      os.close();
+      stringResponse(exchange, 200, response);
     });
   }
 
   public static SimpleServer createGateway() throws IOException {
+
+    HttpClient client = HttpClient.newHttpClient();
     return create(8000, "/gateway/", exchange -> {
 
+      // emulate authentication from header, don't do this in production
+      String secretPrefix = "secret=";
+      String customerAuth = exchange.getRequestHeaders().getFirst("Authorization");
+      String customerId = null;
+      if (customerAuth.startsWith(secretPrefix)) {
+        customerId = customerAuth.substring(secretPrefix.length());
+      }
+      if (customerId == null) {
+        stringResponse(exchange, 401, "auth error");
+        return;
+      }
+
+      HttpRequest request = HttpRequest.newBuilder()
+          .uri(URI.create("http://localhost:9000/backend/"))
+          .header("customer_id", customerId)
+          .build();
+
+      // add new entries to current baggage
+      Baggage baggage = Baggage.current().toBuilder()
+          .put("example.customer.id", customerId)
+          .put("example.customer.name", String.format("my-awesome-customer-%s", customerId))
+          .build();
+
+      // create a new context with the updated baggage and make it current for the backend HTTP call
+      Context contextWithBaggage = baggage.storeInContext(Context.current());
+
+      try (Scope scope = contextWithBaggage.makeCurrent()) {
+
+        // call backend
+        HttpResponse<String> response;
+        try {
+          response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        } catch (InterruptedException e) {
+          stringResponse(exchange, 500, "internal error");
+          return;
+        }
+
+        stringResponse(exchange, 200, response.body());
+      }
     });
+  }
+
+  private static void stringResponse(HttpExchange exchange, int statusCode, String response)
+      throws IOException {
+    exchange.sendResponseHeaders(statusCode, response.length());
+    OutputStream os = exchange.getResponseBody();
+    os.write(response.getBytes());
+    os.close();
   }
 
   public String getUrl() {return this.url;}
