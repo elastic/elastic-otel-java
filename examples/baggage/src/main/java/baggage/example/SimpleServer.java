5ff523727d0e59505983966ce6515d57460ef9f4
Sylvain Juge <763082+SylvainJuge@users.noreply.github.com>
2025-06-26 17:15:16 +0200

    add log statements + comments

--- examples/baggage/src/main/java/baggage/example/SimpleServer.java
+++ examples/baggage/src/main/java/baggage/example/SimpleServer.java
@@ -6,6 +6,8 @@ import com.sun.net.httpserver.HttpServer;
 import io.opentelemetry.api.baggage.Baggage;
 import io.opentelemetry.context.Context;
 import io.opentelemetry.context.Scope;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
@@ -17,6 +19,7 @@ import java.util.concurrent.Executors;
 
 public class SimpleServer {
 
+  private static final Logger log = LoggerFactory.getLogger(SimpleServer.class);
   private final HttpServer server;
   private final String url;
 
@@ -36,9 +39,21 @@ public class SimpleServer {
   public static SimpleServer createBackend() throws IOException {
     return create(9000, "/backend/", exchange -> {
 
-      // Authentication is assumed to be implemented by the gateway
+      // Authentication is assumed to be implemented by the gateway and backend trusts its HTTP header
       // the backend only needs to have a technical ID for customer.
       String customerId = exchange.getRequestHeaders().getFirst("customer_id");
+
+      // Baggage is automatically propagated to the backend, and we can access it through API.
+      // It is not required to modify backend code as it is possible to configure the instrumentation
+      // to automatically propagate some baggage entries to span/log attributes through configuration.
+      //
+      // To enable this, configure the backend with the following JVM arguments:
+      // -Dotel.java.experimental.span-attributes.copy-from-baggage.include=example.customer.id,example.customer.name
+      // -Dotel.java.experimental.span-attributes.copy-from-baggage.include=example.customer.id,example.customer.name
+
+      // This log statement in the backend should only have access to the customer ID.
+      // The log even captured will include the baggage entries, which include both ID and name.
+      log.atInfo().setMessage("backend request for customer ID = " + customerId).log();
 
       String response = String.format("hello from backend, customer_id=%s\n", customerId);
       stringResponse(exchange, 200, response);
@@ -72,13 +87,22 @@ public class SimpleServer {
           .put("example.customer.id", customerId)
           .put("example.customer.name", String.format("my-awesome-customer-%s", customerId))
           .build();
-
       // create a new context with the updated baggage and make it current for the backend HTTP call
       Context contextWithBaggage = baggage.storeInContext(Context.current());
-
       try (Scope scope = contextWithBaggage.makeCurrent()) {
 
-        // call backend
+        // All the log statements and spans created with the baggage-enabled context
+        // will have the baggage entries added as span/log attributes when configured to do so.
+        //
+        // Doing so allows to "annotate" everything that relatest to the given customer in the monitoring
+        // backend with minor code modifications.
+        //
+        // To enable this, configure the gateway with the following JVM arguments:
+        // -Dotel.java.experimental.span-attributes.copy-from-baggage.include=example.customer.id,example.customer.name
+        // -Dotel.java.experimental.span-attributes.copy-from-baggage.include=example.customer.id,example.customer.name
+        log.atInfo().setMessage("gateway request for customer ID = " + customerId).log();
+
+        // call backend and forward its response
         HttpResponse<String> response;
         try {
           response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -86,7 +110,6 @@ public class SimpleServer {
           stringResponse(exchange, 500, "internal error");
           return;
         }
-
         stringResponse(exchange, 200, response.body());
       }
     });
