00b4e0d5335073ccf13ad44b42cda1a598ae2007
SylvainJuge <763082+SylvainJuge@users.noreply.github.com>
2025-07-02 13:34:26 +0200

    create example for baggage (#710)

---
+++ examples/baggage/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+  application
+  java
+
+  id("com.gradleup.shadow") version "8.3.5"
+}
+
+group = "baggage.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation("co.elastic.otel:elastic-otel-runtime-attach:1.4.1")
+  implementation(platform("org.slf4j:slf4j-bom:2.0.16"))
+
+  // using a "real" logger backend as slf4j-simple just uses stdout/stderr
+  // and is not instrumented for log capture by otel instrumentation
+  implementation("org.apache.logging.log4j:log4j-slf4j2-impl:2.25.0")
+
+  // otel API to access Baggage API
+  implementation("io.opentelemetry:opentelemetry-api:1.51.0")
+
+}
+
+application {
+  mainClass = "baggage.example.Main"
+}
+
+tasks {
+  compileJava {
+    options.release.set(21)
+  }
+
+  shadowJar {
+    archiveFileName.set("baggage-example-all.jar")
+  }
+
+  assemble {
+    dependsOn("shadowJar")
+  }
+}
