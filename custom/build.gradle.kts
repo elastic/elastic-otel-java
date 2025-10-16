99fea7db89229dc75f5a4ba8b53ab303a3af358e
Jack Shirazi <jack.shirazi@elastic.co>
2025-10-16 09:29:07 +0100

    remove unnecessary line

--- custom/build.gradle.kts
+++ custom/build.gradle.kts
@@ -78,7 +78,6 @@ tasks {
 tasks.withType<Test>().configureEach {
   val agentJarTask = project(":agent").tasks.named<Jar>("jar")
   dependsOn(agentJarTask)
-  inputs.files(layout.files(agentJarTask))
   systemProperty("elastic.otel.agent.jar.path", agentJarTask.get().archiveFile.get().asFile.absolutePath)
 
   val overrideConfig = project.properties["elastic.otel.overwrite.config.docs"]
