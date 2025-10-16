e213e499bb2c4515b6060a8e0a031db68bfda769
Jack Shirazi <jack.shirazi@elastic.co>
2025-10-16 19:42:43 +0100

    stop windows build failures

--- universal-profiling-integration/src/test/java/co/elastic/otel/UniversalProfilingProcessorAutoConfigTest.java
+++ universal-profiling-integration/src/test/java/co/elastic/otel/UniversalProfilingProcessorAutoConfigTest.java
@@ -34,8 +34,11 @@ import java.util.List;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnOs;
+import org.junit.jupiter.api.condition.OS;
 import org.junit.jupiter.api.io.TempDir;
 
+@DisabledOnOs(OS.WINDOWS)
 public class UniversalProfilingProcessorAutoConfigTest {
 
   @BeforeEach
