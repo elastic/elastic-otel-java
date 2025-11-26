e2923e967b945123ee5ef545fd49a823ac05ef24
Jack Shirazi <jack.shirazi@elastic.co>
2025-11-26 16:34:50 +0000

    log to app at info level

--- custom/src/test/java/co/elastic/otel/config/ConfigLoggingAgentListenerTest.java
+++ custom/src/test/java/co/elastic/otel/config/ConfigLoggingAgentListenerTest.java
@@ -36,6 +36,7 @@ import org.junit.jupiter.api.Test;
 
 public class ConfigLoggingAgentListenerTest {
   static final String TARGET_CLASS_NAME = "TempTest321";
+  static final String LOGGING_PROPERTIES_FILE_NAME = "test_logging.properties";
   static String[] identifyingStrings = {
     "ConfigLoggingAgentListener - AutoConfiguredOpenTelemetrySdk{",
     "tracerProvider=SdkTracerProvider"
@@ -49,6 +50,22 @@ public class ConfigLoggingAgentListenerTest {
     agentJarFile = getAgentJarFile();
     testTargetClass = createTestTarget();
     compileTestTarget(testTargetClass);
+    createLoggingPropertiesFile();
+  }
+
+  private static void createLoggingPropertiesFile() throws IOException {
+    File loggingPropertiesFile =
+        new File(System.getProperty("java.io.tmpdir"), LOGGING_PROPERTIES_FILE_NAME);
+    loggingPropertiesFile.deleteOnExit();
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(loggingPropertiesFile))) {
+      writer.write("handlers=java.util.logging.ConsoleHandler\n");
+      writer.write(".level=INFO\n");
+      writer.write("java.util.logging.ConsoleHandler.level=INFO\n");
+      writer.write(
+          "java.util.logging.ConsoleHandler.formatter=java.util.logging.SimpleFormatter\n");
+      writer.write(
+          "java.util.logging.SimpleFormatter.format=%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %2$s - %5$s%6$s%n\n");
+    }
   }
 
   private static void compileTestTarget(File sourceFile) {
@@ -66,6 +83,7 @@ public class ConfigLoggingAgentListenerTest {
   public static void teardown() throws IOException {
     testTargetClass.delete();
     new File(testTargetClass.getParent(), TARGET_CLASS_NAME + ".class").delete();
+    new File(System.getProperty("java.io.tmpdir"), LOGGING_PROPERTIES_FILE_NAME).delete();
   }
 
   @Test
@@ -88,8 +106,10 @@ public class ConfigLoggingAgentListenerTest {
     List<String> command = new ArrayList<>();
     command.add("java");
     command.add("-Xmx32m");
-    command.add("-Djava.lang.invoke.MethodHandle.CUSTOMIZE_THRESHOLD=-1");
-    command.add("-Djava.lang.invoke.stringConcat=BC_SB");
+    command.add(
+        "-Djava.util.logging.config.file="
+            + new File(System.getProperty("java.io.tmpdir"), LOGGING_PROPERTIES_FILE_NAME)
+                .getAbsolutePath());
     command.add("-Dotel.javaagent.logging=application");
     command.add("-javaagent:" + agentJarFile);
     // Only on false, ie test the 'true' default with no option
